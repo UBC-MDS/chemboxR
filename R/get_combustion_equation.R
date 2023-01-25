7a70a34b502680c1fa5b34d172f4785c86b5d5ff
Wilfred Hass <wthass@student.ubc.ca>
2023-01-25 13:27:16 -0800

    Added more tests and fixed edge cases

--- R/get_combustion_equation.R
+++ R/get_combustion_equation.R
@@ -26,24 +26,45 @@ get_combustion_equation <- function(molecule) {
     stop("Cannot have multiple molecules, don't use parentheses")
   }
 
+  # get molecule counts
+  mol_df <- get_elements(molecule)
+  mol_elem <- mol_df$element
+  CH <- c("C", "H")
+
   # molecule can only contain C and H
-  if (length(setdiff(colnames(mol), c("C", "H"))) != 0){
+  if (length(union(setdiff(CH, mol_elem), setdiff(mol_elem, CH))) != 0 ){
     stop("Molecule can only contain C (carbon) and H (hydrogen)")
   }
 
-  # get molecule counts
-  mol_df <- data.frame(C = 6, H = 14)
-  n_C <- mol_df$C
-  n_H <- mol_df$H
+  for (i in seq_along(mol_df$element)) {
+    elem <- mol_df$element[i]
+    if (elem == "C"){
+      n_C <- mol_df$count[i]
+    }
+    else {
+      n_H <- mol_df$count[i]
+    }
+  }
+
+  # n_C <- mol_df$C
+  # n_H <- mol_df$H
   n_O2 <- (2 * n_C + n_H/2)/2
 
   # create the combustion equation dataframe
   comb_eq <- data.frame(place.holder = 1, O2 = n_O2, CO2 = n_C, H2O = n_H/2)
   names(comb_eq)[colnames(comb_eq) == "place.holder"] <- molecule
-
+  i <- 0
   # multiply by 2 for uneven number of atoms
-  if ((n_O2 + n_C + n_H)%%1 != 0){
+  while ((n_O2 + n_C + n_H)%%1 != 0){
+    if (i > 4){
+      break
+    }
     comb_eq <- comb_eq*2
+    n_C <- comb_eq$CO2
+    n_H <- comb_eq$H2O
+    n_O2 <- (2 * n_C + n_H)/2
+
   }
+
   comb_eq
 }
