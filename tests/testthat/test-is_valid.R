2def79c686624760bee9f3f64c2f4983b2bc3de7
Luke Yang <lukey.ubc@qq.com>
2023-01-30 21:48:39 -0800

    add more tests

--- tests/testthat/test-is_valid.R
+++ tests/testthat/test-is_valid.R
@@ -1,9 +1,13 @@
 # Test for one substance combination
 expect_equal(is_valid('(C2H4)5', TRUE),TRUE)
-# Test for two substance combination
+# Test for two substance combination simple chck
 expect_equal(is_valid('Al2(SO4)3', TRUE),TRUE)
+# Test for two substance combination
+expect_equal(is_valid('Al2(SO4)3'),TRUE)
 # Test for more than two substance combination
 expect_equal(is_valid('Al2(SO4)3(C2H4)5',TRUE),TRUE)
+# test large indices
+expect_equal(is_valid('Al20(SO4)30',TRUE),TRUE)
 # Test for 2-digit substance
 expect_equal(is_valid('C5H12',TRUE),TRUE)
 # Test for multiple 2-digit substance
